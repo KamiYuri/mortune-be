a2c4ee59b34b874956116df12ee0369cbab6c557
ducvq <henryduca2k45pbc@gmail.com>
2023-01-29 16:59:20 +0000

    api create new card list

--- app/Http/Controllers/CardListController.php
+++ app/Http/Controllers/CardListController.php
@@ -3,18 +3,87 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Helpers\Helper;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\CardList;
+use App\Models\Board;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use OpenApi\Attributes\MediaType;
+use OpenApi\Attributes\Post;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\RequestBody;
+use OpenApi\Attributes\Response;
+use OpenApi\Attributes\Schema;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes as OA;
+use App\Traits\HttpResponses;
+
 
 class CardListController extends Controller
 {
+    
+    use HttpResponses;
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
+    #[OA\Get(
+        path: "/card_list", operationId: "card list", summary: "Get all card list in the database",
+        requestBody: new RequestBody
+        (
+            content: [
+                new MediaType(
+                    mediaType: "application/json",
+                    schema: new Schema(
+                        properties: [
+                            
+                        ],
+                        example: ''
+                    ),
+                )
+            ]
+        ),
+        tags: ["CardList"],
+        responses: [
+            new Response(response: 200, description: "Get data card list successfully", content: new JsonContent
+                (
+                    properties:
+                    [
+                        new Property(property: "card_list", properties: [
+                            new Property(property: "id", type: "int"),
+                            new Property(property: "title", type: "string"),
+                            new Property(property: "board_id", type: "id"),
+                            new Property(property: "archived", type: "int"),
+                            new Property(property: "updated_at", type: "string"),
+                            new Property(property: "created_at", type: "string"),
+                        ], type: "object")
+                    ],
+                    example:
+                    [
+                        "card_list" => [
+                            "id" => 1,
+                            "title" => "abc",
+                            "board_id" => 1,
+                            "archived" => 1,
+                            "updated_at" => "2022-11-09T17:55:48.000000Z",
+                            "created_at" => "2022-11-09T17:55:48.000000Z"]
+                    ]
+                ),
+            ),
+            new Response(response: 500, description: "Error in get data workspace"),
+        ],
+    )]
+
     public function index()
     {
-        //
+        $board_id = $request->board_id;
+        return $this->success(CardList::all(), 'OK');
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -23,8 +92,49 @@ class CardListController extends Controller
      */
     public function create()
     {
-        //
+
     }
+
+    #[Post(
+        path: "/card_list", operationId: "cardListAdd", summary: "Add a card list",
+        requestBody: new RequestBody
+        (
+            content: [
+                new MediaType(
+                    mediaType: "application/json",
+                    schema: new Schema(
+                        properties: [
+                            new Property(property: "title", type: "string"),
+                            new Property(property: "board_id", type: "int"),
+                            new Property(property: "archived", type: "int"),
+                        ],
+                        example: ["title" => "do something", "board_id" => 4, "archived" => 0]
+                    ),
+                )
+            ]
+        ),
+        tags: ["CardList"],
+        responses: [
+            new Response(response: 200, description: "Add data card list successfully", content: new JsonContent
+                (
+                    properties:
+                    [
+                        new Property(property: "response", properties: [
+                            new Property(property: "code", type: "int"),
+                            new Property(property: "message", type: "string"),
+                        ], type: "object")
+                    ],
+                    example:
+                    [
+                        "response" => [
+                            "code" => 200,
+                            "message" => "Create card list successfully!"]
+                    ]
+                ),
+            ),
+            new Response(response: 500, description: "Error in add workspace"),
+        ],
+    )]
 
     /**
      * Store a newly created resource in storage.
@@ -35,6 +145,30 @@ class CardListController extends Controller
     public function store(Request $request)
     {
         //
+        try{
+
+            if(is_null($request->board_id) || is_null($request->title)){
+                return $this->error('Missing field!', 401);
+            }
+            
+            $card_list = CardList::where('board_id', $request->board_id)->where('title', $request->title)->first();
+
+            $newCardList = new CardList;
+            $newCardList->title = $request->title;
+            $newCardList->board_id = $request->board_id;
+            $newCardList->archived = $request->archived;
+            
+
+
+            $newCardList->save();
+
+
+            return response()->json(['code' => '200', 
+                'message' => 'Create card list successfully!']);
+            
+        }catch(Exception $error){
+            return $this->error('Error when creating list', 500);
+        }
     }
 
     /**
@@ -46,6 +180,7 @@ class CardListController extends Controller
     public function show($id)
     {
         //
+
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CardListController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('user/authenticated', [UserController::class, "getAuthUser"]);
 
     Route::resource('/user', UserController::class);
+
+    Route::resource('/card_list', CardListController::class);
 });
 
 Route::resource('/task', TaskController::class);
+Route::resource('card_list', CardListController::class);
