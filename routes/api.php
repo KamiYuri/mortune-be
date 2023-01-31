92b25f49488933fabf9efa8ecf5594db6d3bf565
ducvq <henryduca2k45pbc@gmail.com>
2023-01-31 15:08:44 +0000

    CRUD workspace

--- routes/api.php
+++ routes/api.php
@@ -31,11 +31,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::resource('/card_list', CardListController::class);
 
     Route::resource('/card', CardController::class);
+    Route::apiResource('workspace', WorkspaceController::class)->except(['create', 'edit']);
     Route::get('/cardlist/board/{board_id}', [CardListController::class, "getCardListByBoard"]);
     Route::get('/card/cardlist/{list_id}', [CardController::class, "getCardByCardList"]);
 });
 
 Route::resource('/task', TaskController::class);
+Route::apiResource('workspace', WorkspaceController::class)->except(['create', 'edit']);
 Route::get('/workspace/user/{id}', [WorkspaceController::class, "getWorkspaceByUserId"]);
 Route::get('/card/cardlist/{list_id}', [CardController::class, "getCardByCardList"]);
+Route::get('workspace/user/{id}', [WorkspaceController::class, 'getListUserByIdWs']);
+
+
 
